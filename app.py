54f10cf9cb063e2ab767aa5d359800d02e7e7034
8bitnand <8bitnand@gmail.com>
2025-06-27 19:32:12 +0530

    adding workflows

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 from fastapi import FastAPI
+import random
 
 app = FastAPI()
 
@@ -7,3 +8,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"response": "Hello World"}
+
+@app.get("/random")
+def random_number():
+    return {"response": random.randint(0, 100)}
